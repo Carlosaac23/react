9a2c35468271866b0887772cf22c494c6c8f8089
Carlos <carlosaac232001@gmail.com>
2026-05-12 20:20:05 -0500

    chore: create handleSubmit function into HabiForm component

--- src/components/HabitForm.tsx
+++ src/components/HabitForm.tsx
@@ -1,16 +1,38 @@
+import type { SubmitEvent } from 'react';
+
+import { useState } from 'react';
+
 import Button from './Button';
 
-export default function HabitForm() {
+type HabitFormProps = {
+  addHabit: (name: string) => void;
+};
+
+export default function HabitForm({ addHabit }: HabitFormProps) {
+  const [name, setName] = useState('');
+
+  function handleSubmit(e: SubmitEvent) {
+    e.preventDefault();
+
+    if (name.trim() === '') return;
+    setName('');
+    addHabit(name);
+  }
+
   return (
-    <form className='flex gap-2'>
+    <form className='flex gap-2' onSubmit={handleSubmit}>
       <input
-        className='flex-1 rounded bg-neutral-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-violet-500'
+        className='flex-1 rounded-lg bg-neutral-800 px-4 py-2 outline-none focus-visible:ring-2 focus-visible:ring-violet-500'
         type='text'
         placeholder='New habit...'
         name=''
         id=''
+        value={name}
+        onChange={e => setName(e.target.value)}
       />
-      <Button type='submit'>Add Habit</Button>
+      <Button className='px-4 py-2 font-medium' disabled={name.trim() === ''} type='submit'>
+        Add Habit
+      </Button>
     </form>
   );
 }
